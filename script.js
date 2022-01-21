305c8008b181aede3474587f2a080de405f80f24
Dorian Fournier <dorian@openindoor.io>
2022-01-21 16:27:31 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,98 +1,90 @@
-var psList = [];
-var scene, camera, psMain;
+import * as THREE from 'https://cdn.skypack.dev/three@0.129.0';
 
-AFRAME.registerComponent('cross', {
-
-  schema: {
-    index: {type: 'number', default: '-1'}
-  },
-
-  init: function() {
-
-      var data = this.data;
-      var el = this.el;
-
-      el.addEventListener("click", function(event) {
-        psMain.setAttribute("src", psList[data.index].src);
-        addCrossOnPhotosphere(psList[data.index]);
-      });
-
-  }
-
-});
-
-function addCrossOnPhotosphere(origin) {
-  //Move photosphere
-  psMain.setAttribute("position", origin.x + " 0 " + origin.z);
-  camera.setAttribute("position", origin.x + " 0 " + origin.z);
-
-  //Remove ancient cross
-  var listCross = document.querySelectorAll("a-box");
-  listCross.forEach(currentValue => {
-    currentValue.parentNode.removeChild(currentValue);
-  });
-
-  //Add new cross
-  origin.neighbourList.forEach(currentValue => {
-    var index = psList.indexOf(currentValue);
-    var d = Math.sqrt(Math.pow((currentValue.x - origin.x), 2) + Math.pow((currentValue.z - origin.z), 2));
-    var x = Math.round(origin.x + (50/d)*(currentValue.x - origin.x));
-    var z = Math.round(origin.z + (50/d)*(currentValue.z - origin.z));
-
-    var cross = document.createElement("a-box");
-    cross.setAttribute("id", "cross__" + index)
-    cross.setAttribute("position", x + " 0 " + z);
-    cross.setAttribute("scale", "5 5 5");
-    cross.setAttribute("rotation", "45 45 0");
-    cross.setAttribute("cross", "index: "+ index);
-    scene.appendChild(cross);
-  });
-};
-
-function run() {
-  camera = document.querySelector("a-camera");
-  psMain = document.getElementById("main-ps");
-  psMain.setAttribute("src", psList[0].src);
-  addCrossOnPhotosphere(psList[0])
-};
-
-var getValues = function(){
-  scene = document.querySelector("a-scene");
-  if (scene.hasLoaded) {
-    run();
-  } else {
-    scene.addEventListener("loaded", run);
-  }
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
 }
 
-$.getJSON("data_rework.geojson", function(result){
-  var xref, zref, x, z;
-  var factor = 2000000;
-  var data = result.features;
-  data.forEach(currentValue => {
-    if(currentValue.geometry.type == "Point" && currentValue.properties.image == "yes")
+window.addEventListener('resize', () =>
+{
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+// Scene
+const scene = new THREE.Scene()
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const canvas = document.querySelector('canvas.webgl')
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  alpha: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// Sphere object
+
+var arr_sphere = []
+var nmb_sphere = 50
+
+for(let i = 0; i < nmb_sphere; i++)
+{
+  var sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(1,36,36),
+    new THREE.MeshBasicMaterial({ color: 0xffffff })
+  )
+  var rand = Math.random() - 0.5
+  sphere.position.x = rand * 20
+  rand = Math.random() - 0.5
+  sphere.position.y = rand * 10
+  rand = Math.random() - 1
+  sphere.position.z = rand * 100
+  arr_sphere.push(sphere)
+  scene.add(sphere)
+}
+
+// Animation
+var speed = 0.1
+
+function tick()
+{
+  for(let i = 0; i < nmb_sphere; i++)
+  {
+    if(arr_sphere[i].position.z < 10)
     {
-      //Traitement des coordonnées (Decimal degrees -> Useful coordinates)
-      if(psList.length == 0)
-      {
-        xref = currentValue.geometry.coordinates[0];
-        zref = currentValue.geometry.coordinates[1];
-      }
-
-      x = Math.round((currentValue.geometry.coordinates[0] - xref) * factor);
-      z = Math.round((currentValue.geometry.coordinates[1] - zref) * factor);
-
-      psList.push(new Photosphere(x, z, currentValue.properties.src));
+      arr_sphere[i].position.z += speed
     }
-  });
+    else
+    {
+      var rand = Math.random() - 0.5
+      arr_sphere[i].position.x = rand * 20
+      rand = Math.random() - 0.5
+      arr_sphere[i].position.y = rand * 10
+      rand = Math.random() - 1
+      arr_sphere[i].position.z = rand * 100
+    }
+  }
 
-  //Add neighbour
-  psList[0].addNeighbour(psList[1]);
-  psList[1].addNeighbour(psList[0]);
-  psList[1].addNeighbour(psList[2]);
-  psList[2].addNeighbour(psList[1]);
-  psList[2].addNeighbour(psList[3]);
-  psList[3].addNeighbour(psList[2]);
+  renderer.render(scene, camera)
 
-  getValues();
-});
+  window.requestAnimationFrame(tick)
+}
+
+tick();
